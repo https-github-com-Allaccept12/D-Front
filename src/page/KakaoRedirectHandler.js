20ece93f8adad84950df4958c9d9384f935e43e6
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-03-16 01:25:37 +0900

    불필요한 코드 삭제

--- src/page/KakaoRedirectHandler.js
+++ src/page/KakaoRedirectHandler.js
@@ -10,7 +10,6 @@ const KakaoRedirectHandler = props => {
   const history = useHistory();
 
   const code = new URL(window.location.href).searchParams.get('code');
-  console.log('code:', code);
 
   useEffect(() => {
     dispatch(kakaoLoginAxios({code, history}))
