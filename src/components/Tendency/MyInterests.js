d7b2a8df37c4cf8f148c9bb6c95b79db9f2a8af2
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-03-18 05:25:57 +0900

    성향테스트 관심사 설정 업그레이드

--- src/components/Tendency/MyInterests.js
+++ src/components/Tendency/MyInterests.js
@@ -2,6 +2,18 @@ import React, {useState } from "react";
 import { Button, Image } from "../../elements";
 import react_icon from "../../static/images/React-icon.svg.png";
 import { Link } from "react-router-dom";
+import icon from "../../static/images/interests_icon.svg";
+import typography from "../../static/images/typography.svg";
+import crafts from "../../static/images/crafts.svg";
+import ui from "../../static/images/ui.svg";
+import pakage from "../../static/images/pakage.svg";
+import fashion from "../../static/images/fashion.svg";
+import video from "../../static/images/video.svg";
+import product from "../../static/images/product.svg";
+import game from "../../static/images/game.svg";
+import branding from "../../static/images/branding.svg";
+import interior from "../../static/images/interior.svg";
+import graphic from "../../static/images/graphic.svg";
 
 
 const interest = [];
@@ -12,41 +24,50 @@ const MyInterests = (props) => {
   const handleClicked = e => {
     if (interest.length < 1){
       setIsClicked(!isClicked)
-      !isClicked ? e.currentTarget.style.border = "5px solid #9ACD32" : e.currentTarget.style.border = "1px black";
+      !isClicked ? e.currentTarget.style.border = "5px solid #A162F7" : e.currentTarget.style.border = "1px black";
       interest.push(e.currentTarget.innerText)
     } else{
         if (e.currentTarget.innerText === interest[0]){
           setIsClicked(!isClicked)
-          !isClicked ? e.currentTarget.style.border = "5px solid #9ACD32" : e.currentTarget.style.border = "1px black"
+          !isClicked ? e.currentTarget.style.border = "5px solid #A162F7" : e.currentTarget.style.border = "1px black"
           interest.pop();
         }
     }
   }
   
   return (
-    <div>
-      <div className="text-center">관심 분야 1개 선택해 주세요</div>
-      <div className="grid justify-center col-start-1 col-end-8 text-center bg-yellow-400">
-        <div value="typo" className="grid items-center justify-center col-start-1 col-end-4 row-start-1 row-end-3 mx-1 my-1 text-center bg-green-200" onClick={handleClicked}><img src={react_icon}/>타이포그래피</div>
-        <div className="grid items-center justify-center col-start-4 col-end-6 row-start-1 row-end-3 mx-1 my-1 text-center bg-green-400" onClick={handleClicked}><img src={react_icon}/>UI / UX</div>
-        <div className="grid items-center justify-center col-start-6 col-end-8 row-start-1 row-end-4 mx-1 my-1 text-center bg-purple-400" onClick={handleClicked}><img src={react_icon}/>패키지</div>
-        <div className="grid items-center justify-center col-start-1 col-end-3 row-start-3 row-end-5 mx-1 my-1 text-center bg-green-600" onClick={handleClicked}><img src={react_icon}/>그래픽</div>
-        <div className="grid items-center justify-center col-start-3 col-end-6 row-start-3 row-end-6 mx-1 my-1 text-center bg-blue-600" onClick={handleClicked}><img src={react_icon}/>브랜딩 / 편집</div>
-        <div className="grid items-center justify-center col-start-6 col-end-8 row-start-4 row-end-6 mx-1 my-1 text-center bg-red-200" onClick={handleClicked}><img src={react_icon}/>패션</div>
-        <div className="grid items-center justify-center col-start-1 col-end-3 row-start-5 row-end-7 mx-1 my-1 text-center bg-green-400" onClick={handleClicked}><img src={react_icon}/>공예</div>
-        <div className="grid items-center justify-center col-start-3 col-end-5 row-start-6 row-end-7 mx-1 my-1 text-center bg-red-400" onClick={handleClicked}><img src={react_icon}/>제품</div>
-        <div className="grid items-center justify-center col-start-5 col-end-8 row-start-6 row-end-7 mx-1 my-1 text-center bg-blue-400" onClick={handleClicked}><img src={react_icon}/>게임 / 캐릭터</div>
-        <div className="grid items-center justify-center col-start-1 col-end-4 row-start-7 mx-1 my-1 text-center bg-pink-400 row-end-8" onClick={handleClicked}><img src={react_icon}/>영상 / 모션</div>
-        <div className="grid items-center justify-center col-start-4 col-end-8 row-start-7 mx-1 my-1 text-center bg-white row-end-8" onClick={handleClicked}><img src={react_icon}/>건축 / 인테리어 / 환경</div>
-        <div className="grid items-center justify-center col-start-4">
+    <div className="bg-[#A162F7] grid place-content-center">
+      <div><br/></div>
+      <div className="mt-4 bg-white w-fit">
+        <div className="flex my-10 place-content-center">
+          <div className="text-center bg-white">
+            <img src={icon}/>
+            <div>님의 관심사를 알고 싶어요!</div><br/>
+            <div>선택해주신 정보는<br/>아트워크 및 디자이너 추천에<br/>어쩌구 저쩌구 이용됩니다.</div>
+          </div> 
+          <div className="grid col-start-2 col-end-8 text-center bg-white">
+            <div className="grid items-end h-48 col-start-1 col-end-4 row-start-1 row-end-3 px-5 py-4 mx-1 my-1 text-center rounded-md shadow-lg justify-items-start w-74" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={typography}/>타이포그래피</div></div>
+            <div className="grid items-end w-48 h-48 col-start-4 col-end-6 row-start-1 row-end-3 px-6 py-4 mx-1 my-1 text-center rounded-md shadow-lg justify-items-start" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={crafts}/>공예</div></div>
+            <div className="grid items-end w-48 col-start-6 col-end-8 row-start-1 row-end-4 px-6 py-4 mx-1 my-1 mr-8 text-center rounded-md shadow-lg justify-items-start h-74" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={pakage}/>패키지</div></div>
+            <div className="grid items-end w-48 h-48 col-start-1 col-end-3 row-start-3 row-end-5 px-6 py-4 mx-1 my-1 text-center rounded-md shadow-lg justify-items-start" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={graphic}/>그래픽</div></div>
+            <div className="grid items-end col-start-3 col-end-6 row-start-3 row-end-6 px-6 py-4 mx-1 my-1 text-center rounded-md shadow-lg justify-items-start w-74 h-74" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={ui}/>UI / UX</div></div>
+            <div className="grid items-end w-48 h-48 col-start-6 col-end-8 row-start-4 row-end-6 px-6 py-4 mx-1 my-1 mr-8 text-center rounded-md shadow-lg justify-items-start" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={fashion}/>패션</div></div>
+            <div className="grid items-end w-48 col-start-1 col-end-3 row-start-5 row-end-7 px-6 py-4 mx-1 my-1 text-center rounded-md shadow-lg justify-items-start h-74" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={video}/>영상 / 모션</div></div>
+            <div className="grid items-end w-48 h-48 col-start-3 col-end-5 row-start-6 row-end-7 px-6 py-4 mx-1 my-1 text-center rounded-md shadow-lg justify-items-start" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={product}/>제품</div></div>
+            <div className="grid items-end h-48 col-start-5 col-end-8 row-start-6 row-end-7 px-6 py-4 mx-1 my-1 mr-8 text-center rounded-md shadow-lg justify-items-start w-74" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={game}/>게임 / 캐릭터</div></div>
+            <div className="grid items-end h-48 col-start-1 col-end-4 row-start-7 px-6 py-4 mx-1 my-1 text-center rounded-md shadow-lg justify-items-start row-end-8 w-74" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={branding}/>브랜딩 / 편집</div></div>
+            <div className="grid items-end h-48 col-start-4 col-end-8 row-start-7 px-6 py-4 mx-1 my-1 mr-8 text-center rounded-md shadow-lg justify-items-start row-end-8 w-94" onClick={handleClicked}><div className="flex-col my-3"><img className="my-3" src={interior}/>건축 / 인테리어 / 환경</div></div>
+          </div>
+      </div>
+      <div className="flex my-10 place-content-center">
           {interest.length === 1 ? <Link to="/">
             <Button size="1" color="1">확인</Button>
           </Link> : 
-            <Button size="1" color="1">관심사를 선택해주세요.</Button>
+            <Button size="1" color="2">관심사를 선택해주세요.</Button>
           }
-          
         </div>
       </div>
+      <div className="mb-5"> <br/> </div>
     </div>
   );
 };
--- src/components/Tendency/Results.js
+++ src/components/Tendency/Results.js
@@ -14,54 +14,7 @@ const NotFound = (props) => {
 </div>
 <button className="col-start-5"><Link to="/myinterests">다음으로 넘어가기</Link></button>
 <div>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="16" width="43" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="16" width="86" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="130" width="129" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="187" width="172" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="244" width="215" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="301" width="258" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="358" width="301" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="415" width="344" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="472" width="387" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="529" width="430" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="586" width="473" height="10" rx="5" fill="#9262F7"/>
-</svg>
-<svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
-<rect x="16" y="643" width="520" height="10" rx="5" fill="#9262F7"/>
-</svg>
+
 </div>
 </div>
   );
