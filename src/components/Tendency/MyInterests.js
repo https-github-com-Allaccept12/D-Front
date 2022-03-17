3fda2ddc8dfc30e26cdb868062a338197bcf7fe2
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-03-17 23:57:48 +0900

    관심사 설정 구현 완료, 성향테스트 프로그레스바 추가

--- src/components/Tendency/MyInterests.js
+++ src/components/Tendency/MyInterests.js
@@ -1,144 +1,53 @@
-import React from "react";
+import React, {useState } from "react";
 import { Button, Image } from "../../elements";
 import react_icon from "../../static/images/React-icon.svg.png";
-import { Link,  useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+
+const interest = [];
 
 const MyInterests = (props) => {
-  const location = useLocation();
-  const a = location.pathname
-  return (
-<div className="grid col-start-1 col-end-8 bg-yellow-400">
   
-{/* {!a === "/editmyspace" ?
-  <button className="col-start-5 row-start-1"><Link to="/createprofile">다음으로 넘어가기</Link>
-  </button>
-  : ""} */}
-<div className="grid col-start-1 col-end-4 row-start-1 row-end-3 mx-1 my-1 bg-green-200"><img src={react_icon}/></div>
-<div className="grid col-start-4 col-end-6 row-start-1 row-end-3 mx-1 my-1 bg-green-400"><img src={react_icon}/></div>
-<div className="grid col-start-6 col-end-8 row-start-1 row-end-4 mx-1 my-1 bg-purple-400"><img src={react_icon}/></div>
-<div className="grid col-start-1 col-end-3 row-start-3 row-end-5 mx-1 my-1 bg-green-600"><img src={react_icon}/></div>
-<div className="grid col-start-3 col-end-6 row-start-3 row-end-6 mx-1 my-1 bg-blue-600"><img src={react_icon}/></div>
-<div className="grid col-start-6 col-end-8 row-start-4 row-end-6 mx-1 my-1 bg-red-200"><img src={react_icon}/></div>
-<div className="grid col-start-1 col-end-3 row-start-5 row-end-7 mx-1 my-1 bg-green-400"><img src={react_icon}/></div>
-<div className="grid col-start-3 col-end-5 row-start-6 row-end-7 mx-1 my-1 bg-red-400"><img src={react_icon}/></div>
-<div className="grid col-start-5 col-end-8 row-start-6 row-end-7 mx-1 my-1 bg-blue-400"><img src={react_icon}/></div>
-<div className="grid col-start-1 col-end-4 row-start-7 mx-1 my-1 bg-pink-400 row-end-8"><img src={react_icon}/></div>
-<div className="grid col-start-4 col-end-8 row-start-7 mx-1 my-1 bg-white row-end-8"><img src={react_icon}/></div>
-
-    {/* <div className="">
-        관심사를 정해주세여
-    </div>
-          <div className="">
-
-<section className="text-gray-600 body-font ">
-  <div className="container px-5 py-2 mx-auto">
-
-    <div className="flex flex-wrap -m-4">
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/600x360" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Shooting Stars</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/601x361" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">The Catalyzer</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/603x363" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">The 400 Blows</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/602x362" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Neptune</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/605x365" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Holden Caulfield</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/606x366" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Alper Kamu</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/606x366" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Alper Kamu</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/606x366" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Alper Kamu</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 lg:w-1/3 sm:w-1/2">
-        <div className="relative flex">
-          <img alt="gallery" className="absolute inset-0 object-cover object-center w-full h-full" src="https://dummyimage.com/606x366" />
-          <div className="relative z-10 w-full px-8 py-10 bg-white border-4 border-gray-200 opacity-0 hover:opacity-100">
-            <h2 className="mb-1 text-sm font-medium tracking-widest text-indigo-500 title-font">THE SUBTITLE</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Alper Kamu</h1>
-            <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
+  const [isClicked, setIsClicked] = useState(false);
+  const handleClicked = e => {
+    if (interest.length < 1){
+      setIsClicked(!isClicked)
+      !isClicked ? e.currentTarget.style.border = "5px solid #9ACD32" : e.currentTarget.style.border = "1px black";
+      interest.push(e.currentTarget.innerText)
+    } else{
+        if (e.currentTarget.innerText === interest[0]){
+          setIsClicked(!isClicked)
+          !isClicked ? e.currentTarget.style.border = "5px solid #9ACD32" : e.currentTarget.style.border = "1px black"
+          interest.pop();
+        }
+    }
+  }
+  
+  return (
+    <div>
+      <div className="text-center">관심 분야 1개 선택해 주세요</div>
+      <div className="grid justify-center col-start-1 col-end-8 text-center bg-yellow-400">
+        <div value="typo" className="grid items-center justify-center col-start-1 col-end-4 row-start-1 row-end-3 mx-1 my-1 text-center bg-green-200" onClick={handleClicked}><img src={react_icon}/>타이포그래피</div>
+        <div className="grid items-center justify-center col-start-4 col-end-6 row-start-1 row-end-3 mx-1 my-1 text-center bg-green-400" onClick={handleClicked}><img src={react_icon}/>UI / UX</div>
+        <div className="grid items-center justify-center col-start-6 col-end-8 row-start-1 row-end-4 mx-1 my-1 text-center bg-purple-400" onClick={handleClicked}><img src={react_icon}/>패키지</div>
+        <div className="grid items-center justify-center col-start-1 col-end-3 row-start-3 row-end-5 mx-1 my-1 text-center bg-green-600" onClick={handleClicked}><img src={react_icon}/>그래픽</div>
+        <div className="grid items-center justify-center col-start-3 col-end-6 row-start-3 row-end-6 mx-1 my-1 text-center bg-blue-600" onClick={handleClicked}><img src={react_icon}/>브랜딩 / 편집</div>
+        <div className="grid items-center justify-center col-start-6 col-end-8 row-start-4 row-end-6 mx-1 my-1 text-center bg-red-200" onClick={handleClicked}><img src={react_icon}/>패션</div>
+        <div className="grid items-center justify-center col-start-1 col-end-3 row-start-5 row-end-7 mx-1 my-1 text-center bg-green-400" onClick={handleClicked}><img src={react_icon}/>공예</div>
+        <div className="grid items-center justify-center col-start-3 col-end-5 row-start-6 row-end-7 mx-1 my-1 text-center bg-red-400" onClick={handleClicked}><img src={react_icon}/>제품</div>
+        <div className="grid items-center justify-center col-start-5 col-end-8 row-start-6 row-end-7 mx-1 my-1 text-center bg-blue-400" onClick={handleClicked}><img src={react_icon}/>게임 / 캐릭터</div>
+        <div className="grid items-center justify-center col-start-1 col-end-4 row-start-7 mx-1 my-1 text-center bg-pink-400 row-end-8" onClick={handleClicked}><img src={react_icon}/>영상 / 모션</div>
+        <div className="grid items-center justify-center col-start-4 col-end-8 row-start-7 mx-1 my-1 text-center bg-white row-end-8" onClick={handleClicked}><img src={react_icon}/>건축 / 인테리어 / 환경</div>
+        <div className="grid items-center justify-center col-start-4">
+          {interest.length === 1 ? <Link to="/">
+            <Button size="1" color="1">확인</Button>
+          </Link> : 
+            <Button size="1" color="1">관심사를 선택해주세요.</Button>
+          }
+          
         </div>
       </div>
     </div>
-  </div>
-</section>
-
-
-
-                </div>
-
-                
-            */}
-
-
-
-</div>
   );
 };
 
--- src/components/Tendency/TendencyTest.js
+++ src/components/Tendency/TendencyTest.js
@@ -31,118 +31,186 @@ const TendencyTest = (props) => {
     
   return (
     <div className="grid items-center w-full max-h-screen grid-cols-7 grid-rows-6 bg-yellow-400">
-      <div className="col-start-4 row-start-2">디자이너 성향테스트</div>
-      {TestNum === 0 && <div className="col-start-4 row-start-3">
+      {TestNum === 0 && 
+      <div className="col-start-4 row-start-2 text-center">디자이너 성향테스트
+        <div className="col-start-4 row-start-3 text-center"><br/>
           <div>나의 디자인 성향은?</div>
           <div>1분 만에 테스트 하기!</div>
         </div>
+      </div>
       }
-      {TestNum === 1 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 1 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="43" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">1. 회사 첫 출근일에 내 자리를 세팅한 후</div><br/>
             <Button size="1" color="1" onClick={()=>{mbti.E += 1; nextTestNum(TestNum + 1);}}>
               주변 사람들에게 먼저 말을 걸어본다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{mbti.I += 1; nextTestNum(TestNum + 1);}}>
               다른 사람들이 먼저 말을 걸 때까지 기다린다.</Button>
-          </div>
+        </div>
         }
-      {TestNum === 2 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 2 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="86" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">2. 재밌을 것 같은 아이디어가 번뜩 떠올랐을 때 나는</div><br/>
             <Button size="1" color="1" onClick={()=>{mbti.E += 1; nextTestNum(TestNum + 1);}}>
               누구랑 같이 해보면 좋을까? 카톡창에서 같이할 팀원을 찾아본다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{mbti.I += 1; nextTestNum(TestNum + 1);}}>
               혼자가 편하지! 나만의 계획에 따라 진행한다.</Button>
-          </div>
+        </div>
         }  
-      {TestNum === 3 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 3 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="129" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">3. 아무리 디자인 작업을 해도 어딘가 만족스럽지 못 할 때</div><br/>
             <Button size="1" color="1" onClick={()=>{mbti.E += 1; nextTestNum(TestNum + 1);}}>
               사수나 동료에게 피드백을 요청한다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{mbti.I += 1; nextTestNum(TestNum + 1);}}>
               D+, 비핸스, 핀터레스트 등 계속 래퍼런스를 찾아본다.</Button>
-          </div>
+        </div>
         }
-      {TestNum === 4 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 4 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="172" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">4. 오늘 퇴근 전까지 급하게 시안을 바꿔야 한다면</div><br/>
             <Button size="1" color="1" onClick={()=>{mbti.S += 1; nextTestNum(TestNum + 1);}}>
               이전에 했던 작업 중에서 최대한 있어보이게 응용한다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{mbti.N += 1; nextTestNum(TestNum + 1);}}>
               시간 없어도 최대한 새로운 아이디어로 수정한다.</Button>
-          </div>
+        </div>
         }   
-      {TestNum === 5 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 5 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="215" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">5. 내가 선호하는 피드백은</div><br/>
             <Button size="1" color="1" onClick={()=>{mbti.S += 1; nextTestNum(TestNum + 1);}}>
               오른쪽으로 1px 옮겨주시고 색상은 #~로 수정해주세요.</Button><br/>
             <Button size="1" color="1" onClick={()=>{mbti.N += 1; nextTestNum(TestNum + 1);}}>
               ~한 느낌으로 적당히 수정해주세요.</Button>
-          </div>
+        </div>
         }   
-      {TestNum === 6 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 6 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="258" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">6. 마감기한이 다가오는데 작업하기 싫을 때</div><br/>
             <Button size="1" color="1" onClick={()=>{mbti.S += 1; nextTestNum(TestNum + 1);}}>
               이유는 없다. 그냥 하기 싫다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{mbti.N += 1; nextTestNum(TestNum + 1);}}>
-              내가 이 일을 왜 해야하지? 하다가 도망가면 어떻게 될까? 상상의 나래 시작</Button>
-          </div>
+              내가 이 일을 왜 해야하지? 도망가면 어떻게 될까? 상상의 나래 시작</Button>
+        </div>
         }   
-      {TestNum === 7 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 7 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="301" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">7. 내가 선호하는 작업 방식은</div><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.T += 1; nextTestNum(TestNum + 1);}}>
               그리드를 딱딱 맞추며 가이드라인에 충실한다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.F += 1; nextTestNum(TestNum + 1);}}>
               가이드라인에는 안맞아도 내 느낌! 감성! 으로 한다.</Button>
-          </div>
+        </div>
         }   
-      {TestNum === 8 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 8 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="344" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">8. 디자인할 때 내가 자신있는 부분은</div><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.T += 1; nextTestNum(TestNum + 1);}}>
               논리적이고 설득력있는 기획</Button><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.F += 1; nextTestNum(TestNum + 1);}}>
               개성있고 창의적인 아이디어</Button>
-          </div>
+        </div>
         }   
-      {TestNum === 9 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 9 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="387" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">9. 동료 디자이너가 새로운 툴을 추천해줬을 때 나는</div><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.T += 1; nextTestNum(TestNum + 1);}}>
               왜 좋은 툴이지? 어떨 때 쓰면 좋은지 서치햅고 결정</Button><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.F += 1; nextTestNum(TestNum + 1);}}>
               좋다니까 한 번 써보자! 일단 써보고 계속 쓸지 말지 결정</Button>
-          </div>
+        </div>
         }   
-      {TestNum === 10 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 10 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="430" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">10. 디자인 작업할 때 내 파일은</div><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.J += 1; nextTestNum(TestNum + 1);}}>
               언제나 규칙적으로! 정해진 파일명으로 폴더에 정리한다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.P += 1; nextTestNum(TestNum + 1);}}>
-              최종.psd, 제발최종.psd 그 때 그 때 내 기분에 따라 바탕화면에 널려 있다.</Button>
-          </div>
+              최종.psd, 제발최종.psd.. 기분에 따라 바탕화면에 널려 있다.</Button>
+        </div>
         }   
-      {TestNum === 11 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 11 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="473" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">11. 내 포트폴리오에 넣고 싶은 작업은</div><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.J += 1; nextTestNum(TestNum + 1);}}>
               포트폴리오 사이트에서 사람들에게 인기가 많았던 프로젝트</Button><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.P += 1; nextTestNum(TestNum + 1);}}>
               고생했지만 많이 성장할 수 있었던 애증의 프로젝트</Button>
-          </div>
+        </div>
         }   
-      {TestNum === 12 && <div className="grid items-center col-start-2 col-end-7 row-start-3">
+      {TestNum === 12 && 
+        <div className="grid items-center justify-center col-start-2 col-end-7 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="495" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
           <div className="text-center">12. '응닶 없음' 이 떴을 때 나는</div><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.J += 1; nextTestNum(TestNum + 1);}}>
               Ctrl + S 는 생명! 이미 저장했다.</Button><br/>
             <Button size="1" color="1" onClick={()=>{ mbti.P += 1; nextTestNum(TestNum + 1);}}>
               시간을 삭제 당했다. 울면서 처음부터 새로 작업해야 한다.</Button>
-          </div>
+        </div>
         }
-      {TestNum >= 13 && <div className="grid items-center col-start-2 col-end-7 row-start-3 mx-auto">
-          <div className="text-center">"{designation}"<br/> 칭호를 얻으셨습니다!</div><br/>
-              <Link to="/">
+      {TestNum >= 13 && 
+        <div className="grid items-center justify-center col-start-4 col-end-5 row-start-3">
+          <svg width="552" height="50" viewBox="0 0 552 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21C16 18.2386 18.2386 16 21 16H531C533.761 16 536 18.2386 536 21V21C536 23.7614 533.761 26 531 26H21C18.2386 26 16 23.7614 16 21V21Z" fill="#EDF1F7"/>
+            <rect x="16" y="16" width="520" height="10" rx="5" fill="#9262F7"/>
+          </svg><br/><br/><br/>
+          <div className="text-center">"{designation}"<br/><br/> 칭호를 얻으셨습니다!</div><br/>
+              <Link to="/MyInterests">
+                <div className="grid items-center col-start-4 col-end-5 row-start-3">
                 <Button size="1" color="1">
                     확인
-                </Button>
+                </Button></div>
               </Link>
-          </div>
+        </div>
         }          
-      <div className="col-start-4 row-start-5">
+      <div className="grid items-center col-start-4 row-start-5">
           {TestNum === 0 && <Button size="2" color="3" onClick={()=>{nextTestNum(TestNum + 1)}}>
             시작하기
           </Button>
