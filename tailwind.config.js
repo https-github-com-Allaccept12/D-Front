cc3f08fc5650a442f67572b983f033911d26c90e
matty255 <89088205+matty255@users.noreply.github.com>
2022-03-18 12:52:57 +0900

    test05

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}", './node_modules/tw-elements/dist/js/**/*.js'
+    "./src/**/*.{js,jsx,ts,tsx,html}", './node_modules/tw-elements/dist/js/**/*.js' 
   ],
   theme: {
     
