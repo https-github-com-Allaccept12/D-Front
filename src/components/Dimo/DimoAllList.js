f99b9ab14de17873253689291093d17f329d58c2
matty255 <89088205+matty255@users.noreply.github.com>
2022-03-24 10:04:20 +0900

    탭커밋

--- src/components/Dimo/DimoAllList.js
+++ src/components/Dimo/DimoAllList.js
@@ -15,13 +15,19 @@ const DimoAllList = (props) => {
   let history = useHistory();
   const location = useLocation();
   const a = location.pathname
-  const b = a.split("/")[2]
-  
-  if(b === "mywork") return (
+  const b = a.split("/")[3]
+  console.log(b)
+  if(b === "all") return (
     <>
 
     <Grid>
-
+    <div className="flex items-center w-full justify-around space-x-36">
+      <div className="box-border rounded-full 
+      bg-white border border-black h-[2.5rem]
+      flex justify-center items-center px-10 my-5"
+      ><button>인기순</button> <button>최신순</button> <button>팔로우디자이너</button></div> 
+      <SearchInput />
+      </div>
       <hr className="border w-full mb-10" />
         {p.map(n => { return <DimoWorkPost /> }) }
         </Grid>
@@ -29,7 +35,7 @@ const DimoAllList = (props) => {
       );
 
       
-  if(b === "community") return (
+  if(b === "uiux") return (
     <>
     <div className="bg-dgray-200 h-[3.7rem] w-full"></div>
     <Grid>
--- src/page/ArtWork.js
+++ src/page/ArtWork.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ArtWorkAllList, ArtWorkCategory, ArtWorkCreate, ArtWorkInlineDetail } from "../components/ArtWorks";
+import { ArtWorkAllList, ArtWorkCategory, ArtWorkCreate, ArtWorkInlineDetail, ArtWorkWrite } from "../components/ArtWorks";
 import { Title, Button } from "../elements"
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -13,10 +13,21 @@ const ArtWork = (props) => {
   return (
     <>
     <div className="bg-dgray-200">
-      {/* <ArtWorkCategory /> */}
+
+    <div className='grid grid-cols-4'>
+        <div className='md:ml-28 mt-4 w-40 h-[112.5rem] '>
+        
+        <ArtWorkCategory />
+
+        </div>
+     
+    <div className="col-start-2 col-end-5 row-start-1">
+        <ArtWorkAllList  />
+        </div>
+        </div>
     <Switch>
         <Route exact path="/art/list/:name" component={ArtWorkAllList} />
-        <Route exact path="/art/createart" component={ArtWorkCreate} />
+       
         <Route exact path="/art/detail" component={ArtWorkInlineDetail} />
         </Switch>
         </div>
