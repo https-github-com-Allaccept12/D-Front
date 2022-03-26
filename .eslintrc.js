5c365cdd5ae70b7c3b49db759901776e44898c72
matty255 <89088205+matty255@users.noreply.github.com>
2022-03-26 18:24:51 +0900

    끝!

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,15 @@ module.exports = {
     },
     extends: [
         "prettier",
+        "javascriptreact",
         // prettier 관련 규칙 모음
     ],
 
     rules: {
         // 추가하고 싶은 rule을 더 추가해줍니다.
+    },
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
     },
 };
