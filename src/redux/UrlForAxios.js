11b2b5e972055a12cc48af0f5e3fff24c6198630
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-03-26 21:36:39 +0900

    토큰 처리 수정

--- src/redux/UrlForAxios.js
+++ src/redux/UrlForAxios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
-// import { getCookie } from "../shared/cookie";
+import { getCookie } from "../shared/cookie";
 
 const URL = axios.create({
   baseURL: process.env.REACT_APP_URL,
   headers: {},
 });
 
-// const token = getCookie("access_token");
-const token = sessionStorage.getItem("access_token");
-const account_id = sessionStorage.getItem("account_id");
+const token = getCookie("access_token");
+// const token = sessionStorage.getItem("access_token");
+// const account_id = sessionStorage.getItem("account_id");
+const account_id = getCookie("account_id");
 
 export { URL, token, account_id };
