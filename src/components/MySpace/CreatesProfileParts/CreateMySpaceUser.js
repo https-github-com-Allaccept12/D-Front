ccffa6ae02ce4a00f089eff77177dd78637c8a0a
matty255 <89088205+matty255@users.noreply.github.com>
2022-04-02 19:21:55 +0900

    job options value

--- src/components/MySpace/CreatesProfileParts/CreateMySpaceUser.js
+++ src/components/MySpace/CreatesProfileParts/CreateMySpaceUser.js
@@ -64,17 +64,17 @@ const CreateMySpaceUser = (props) => {
     const [nicknameState, setNicknameState] = useState("");
 
     const JobOptions = [
-        { value: "UI & UX", label: "UI & UX" },
-        { value: "패션", label: "패션" },
-        { value: "타이포그래피", label: "타이포그래피" },
-        { value: "공예", label: "공예" },
-        { value: "패키지", label: "패키지" },
-        { value: "그래픽", label: "그래픽" },
-        { value: "영상/모션", label: "영상/모션" },
-        { value: "제품", label: "제품" },
-        { value: "게임/캐릭터", label: "게임/캐릭터" },
-        { value: "브랜딩/편집", label: "브랜딩/편집" },
-        { value: "건축/인테리어/환경", label: "건축/인테리어/환경" },
+        { value: "uiux", label: "UI & UX" },
+        { value: "fashion", label: "패션" },
+        { value: "typo", label: "타이포그래피" },
+        { value: "craft", label: "공예" },
+        { value: "package", label: "패키지" },
+        { value: "graphic", label: "그래픽" },
+        { value: "video", label: "영상/모션" },
+        { value: "product", label: "제품" },
+        { value: "game", label: "게임/캐릭터" },
+        { value: "edit", label: "브랜딩/편집" },
+        { value: "eco", label: "건축/인테리어/환경" },
     ];
 
     const [selected, setSelected] = useState("");
