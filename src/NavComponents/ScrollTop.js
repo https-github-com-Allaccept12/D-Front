9f9f815a57802ddbf894cd5450bea49b4b684dfb
matty255 <89088205+matty255@users.noreply.github.com>
2022-04-09 20:11:20 +0900

    스크롤 적용

--- src/NavComponents/ScrollTop.js
+++ src/NavComponents/ScrollTop.js
@@ -3,8 +3,20 @@ import { useLocation } from "react-router-dom";
 
 export default function ScollTop() {
     const { pathname } = useLocation();
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+    if (pathname.startsWith("/dimo")) {
+        useEffect(() => {
+            window.scrollTo(0, 500);
+        }, [pathname]);
+        return null;
+    } else if (pathname.startsWith("/art")) {
+        useEffect(() => {
+            window.scrollTo(0, 200);
+        }, [pathname]);
+    } else if (pathname.startsWith("/myspace")) {
+        useEffect(() => {
+            window.scrollTo(0, 800);
+        }, [pathname]);
+    }
+
     return null;
 }
